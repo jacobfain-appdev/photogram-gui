0948c19d2550e7413fded77b1da01ff23d1aeb41
jacobfain <jfain@chicagobooth.edu>
2021-05-18 22:55:48 +0000

    create photo details page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,10 @@ class PhotosController<ApplicationController
     @list_of_photos = matching_photos.order({:created_at=>:desc})
     render({:template => "photo_templates/index.html.erb"})
   end
+  def show
+    photo_id = params.fetch("path_id")
+    matching_photo = Photo.where({:id => photo_id})
+    @the_photo = matching_photo.first
+    render({:template => "photo_templates/show.html.erb"})
+  end
 end
